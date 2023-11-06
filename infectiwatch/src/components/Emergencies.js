99d83a09a897995e9c50ff3c549a6981dba1b4e3
Tim <mohamud.hussein@student.moringaschool.com>
2023-11-06 20:45:37 +0300

    done

---
+++ infectiwatch/src/components/Emergencies.js
@@ -0,0 +1,27 @@
+// About.js
+
+import React from 'react';
+import './Emergencies.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h2>About Us</h2>
+      <p>
+        Welcome to our website! We are dedicated to providing you with the best
+        experience possible.
+      </p>
+      <div className="contact-info">
+        <p>Phone: (123) 456-7890</p>
+        <p>Email: info@example.com</p>
+      </div>
+      <div className="social-links">
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Instagram</a>
+      </div>
+    </div>
+  );
+};
+
+export default Emergencies;
